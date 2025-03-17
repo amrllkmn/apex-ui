727396f2a153a0a69132ead447efd92327616bf7
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-03-17 16:59:55 +0800

    feat: new copy

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -1,1 +1,4 @@
-<h1>Activites</h1>
+<h1 class="mb-4 text-4xl font-bold tracking-tight md:text-6xl">It's coming.</h1>
+<p class="mx-auto mb-8 max-w-2xl text-lg text-muted-foreground md:text-xl">
+	Max out your potential and put yourself at the top.
+</p>
