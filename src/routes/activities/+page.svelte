815dd66fd893919897a27628bc7bd38962a02734
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-03-18 14:54:43 +0800

    feat: add link to demo

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -1,6 +1,9 @@
 <div class="container mx-auto px-4 text-center">
 	<h1 class="mb-4 text-4xl font-bold tracking-tight md:text-6xl">It's coming.</h1>
 	<p class="mx-auto mb-8 max-w-2xl text-lg text-muted-foreground md:text-xl">
-		Max out your potential and put yourself at the top.
+		Max out your potential and put yourself at the top. <a
+			href="/demo"
+			class="underline hover:text-foreground">Try it out</a
+		>.
 	</p>
 </div>
