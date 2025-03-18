4306b227a3cd2bdc877927fdcb85f349ea876f32
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-03-18 17:37:49 +0800

    feat: add daily, weekly, monthly

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -6,12 +6,76 @@
 	import { Label } from '$lib/components/ui/label/index.js';
 </script>
 
-<Tabs.Root value="account" class="w-full sm:w-1/2">
-	<Tabs.List class="grid w-full grid-cols-2">
-		<Tabs.Trigger value="account">Account</Tabs.Trigger>
-		<Tabs.Trigger value="password">Password</Tabs.Trigger>
+<Tabs.Root value="daily" class="w-full sm:w-1/2">
+	<Tabs.List class="grid w-full grid-cols-3">
+		<Tabs.Trigger value="daily">Daily</Tabs.Trigger>
+		<Tabs.Trigger value="weekly">Weekly</Tabs.Trigger>
+		<Tabs.Trigger value="monthly">Monthly</Tabs.Trigger>
 	</Tabs.List>
-	<Tabs.Content value="account">
+	<Tabs.Content value="daily" class="space-y-4">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Account</Card.Title>
+				<Card.Description>
+					Make changes to your account here. Click save when you're done.
+				</Card.Description>
+			</Card.Header>
+			<Card.Content class="space-y-2">
+				<div class="space-y-1">
+					<Label for="name">Name</Label>
+					<Input id="name" value="Pedro Duarte" />
+				</div>
+				<div class="space-y-1">
+					<Label for="username">Username</Label>
+					<Input id="username" value="@peduarte" />
+				</div>
+			</Card.Content>
+			<Card.Footer>
+				<Button>Save changes</Button>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Account</Card.Title>
+				<Card.Description>
+					Make changes to your account here. Click save when you're done.
+				</Card.Description>
+			</Card.Header>
+			<Card.Content class="space-y-2">
+				<div class="space-y-1">
+					<Label for="name">Name</Label>
+					<Input id="name" value="Pedro Duarte" />
+				</div>
+				<div class="space-y-1">
+					<Label for="username">Username</Label>
+					<Input id="username" value="@peduarte" />
+				</div>
+			</Card.Content>
+			<Card.Footer>
+				<Button>Save changes</Button>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Account</Card.Title>
+				<Card.Description>
+					Make changes to your account here. Click save when you're done.
+				</Card.Description>
+			</Card.Header>
+			<Card.Content class="space-y-2">
+				<div class="space-y-1">
+					<Label for="name">Name</Label>
+					<Input id="name" value="Pedro Duarte" />
+				</div>
+				<div class="space-y-1">
+					<Label for="username">Username</Label>
+					<Input id="username" value="@peduarte" />
+				</div>
+			</Card.Content>
+			<Card.Footer>
+				<Button>Save changes</Button>
+			</Card.Footer>
+		</Card.Root>
 		<Card.Root>
 			<Card.Header>
 				<Card.Title>Account</Card.Title>
@@ -34,7 +98,7 @@
 			</Card.Footer>
 		</Card.Root>
 	</Tabs.Content>
-	<Tabs.Content value="password">
+	<Tabs.Content value="weekly">
 		<Card.Root>
 			<Card.Header>
 				<Card.Title>Password</Card.Title>
@@ -57,89 +121,7 @@
 			</Card.Footer>
 		</Card.Root>
 	</Tabs.Content>
-</Tabs.Root>
-
-<Tabs.Root value="account" class="w-full sm:w-1/2">
-	<Tabs.List class="grid w-full grid-cols-2">
-		<Tabs.Trigger value="account">Account</Tabs.Trigger>
-		<Tabs.Trigger value="password">Password</Tabs.Trigger>
-	</Tabs.List>
-	<Tabs.Content value="account">
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Account</Card.Title>
-				<Card.Description>
-					Make changes to your account here. Click save when you're done.
-				</Card.Description>
-			</Card.Header>
-			<Card.Content class="space-y-2">
-				<div class="space-y-1">
-					<Label for="name">Name</Label>
-					<Input id="name" value="Pedro Duarte" />
-				</div>
-				<div class="space-y-1">
-					<Label for="username">Username</Label>
-					<Input id="username" value="@peduarte" />
-				</div>
-			</Card.Content>
-			<Card.Footer>
-				<Button>Save changes</Button>
-			</Card.Footer>
-		</Card.Root>
-	</Tabs.Content>
-	<Tabs.Content value="password">
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Password</Card.Title>
-				<Card.Description>
-					Change your password here. After saving, you'll be logged out.
-				</Card.Description>
-			</Card.Header>
-			<Card.Content class="space-y-2">
-				<div class="space-y-1">
-					<Label for="current">Current password</Label>
-					<Input id="current" type="password" />
-				</div>
-				<div class="space-y-1">
-					<Label for="new">New password</Label>
-					<Input id="new" type="password" />
-				</div>
-			</Card.Content>
-			<Card.Footer>
-				<Button>Save password</Button>
-			</Card.Footer>
-		</Card.Root>
-	</Tabs.Content>
-</Tabs.Root>
-<Tabs.Root value="account" class="w-full sm:w-1/2">
-	<Tabs.List class="grid w-full grid-cols-2">
-		<Tabs.Trigger value="account">Account</Tabs.Trigger>
-		<Tabs.Trigger value="password">Password</Tabs.Trigger>
-	</Tabs.List>
-	<Tabs.Content value="account">
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Account</Card.Title>
-				<Card.Description>
-					Make changes to your account here. Click save when you're done.
-				</Card.Description>
-			</Card.Header>
-			<Card.Content class="space-y-2">
-				<div class="space-y-1">
-					<Label for="name">Name</Label>
-					<Input id="name" value="Pedro Duarte" />
-				</div>
-				<div class="space-y-1">
-					<Label for="username">Username</Label>
-					<Input id="username" value="@peduarte" />
-				</div>
-			</Card.Content>
-			<Card.Footer>
-				<Button>Save changes</Button>
-			</Card.Footer>
-		</Card.Root>
-	</Tabs.Content>
-	<Tabs.Content value="password">
+	<Tabs.Content value="monthly">
 		<Card.Root>
 			<Card.Header>
 				<Card.Title>Password</Card.Title>
