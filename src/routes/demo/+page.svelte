427ee51aa94dd0c77aad77c9639f996ae73e7827
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-03-29 15:14:56 +0800

    feat: remove weekly and monthly analytics

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -14,10 +14,8 @@
 </script>
 
 <Tabs.Root value="daily" class="w-full sm:w-1/2">
-	<Tabs.List class="grid w-full grid-cols-3">
+	<Tabs.List class="grid w-full grid-cols-1">
 		<Tabs.Trigger value="daily">Daily</Tabs.Trigger>
-		<Tabs.Trigger value="weekly">Weekly</Tabs.Trigger>
-		<Tabs.Trigger value="monthly">Monthly</Tabs.Trigger>
 	</Tabs.List>
 	<Tabs.Content value="daily" class="space-y-4">
 		<!-- TODO: Move to separate component -->
@@ -75,51 +73,5 @@
 				</form>
 			</Dialog.Content>
 		</Dialog.Root>
-	</Tabs.Content>
-	<Tabs.Content value="weekly">
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Password</Card.Title>
-				<Card.Description>
-					Change your password here. After saving, you'll be logged out.
-				</Card.Description>
-			</Card.Header>
-			<Card.Content class="space-y-2">
-				<div class="space-y-1">
-					<Label for="current">Current password</Label>
-					<Input id="current" type="password" />
-				</div>
-				<div class="space-y-1">
-					<Label for="new">New password</Label>
-					<Input id="new" type="password" />
-				</div>
-			</Card.Content>
-			<Card.Footer>
-				<Button>Save password</Button>
-			</Card.Footer>
-		</Card.Root>
-	</Tabs.Content>
-	<Tabs.Content value="monthly">
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Password</Card.Title>
-				<Card.Description>
-					Change your password here. After saving, you'll be logged out.
-				</Card.Description>
-			</Card.Header>
-			<Card.Content class="space-y-2">
-				<div class="space-y-1">
-					<Label for="current">Current password</Label>
-					<Input id="current" type="password" />
-				</div>
-				<div class="space-y-1">
-					<Label for="new">New password</Label>
-					<Input id="new" type="password" />
-				</div>
-			</Card.Content>
-			<Card.Footer>
-				<Button>Save password</Button>
-			</Card.Footer>
-		</Card.Root>
 	</Tabs.Content>
 </Tabs.Root>
